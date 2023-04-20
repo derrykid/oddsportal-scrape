a1694d237ebe6b821189e7024d3c1faa985c4f32
derrykid <derry.yeh.dev@gmail.com>
2023-04-20 15:24:49 +0100

    season

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 from scrap_func import *
 import os
 
 scrape_sport_links_main(sport='basketball',
                         country='usa',
                         league='nba',
-                        start_season='2008',
+                        start_season='2008-2022',
                         nseasons=14,
                         max_page=28)
